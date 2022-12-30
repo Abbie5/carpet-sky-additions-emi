ecf32da4cc9eec3fb182898f5bad3bf3e084d561
Abigail Ient <abigail@ient.me>
2022-12-30 15:36:13 +0000

    actually switch to mojmap

--- src/main/java/cc/abbie/carpetskyadditionsemi/EmiPlugin.java
+++ src/main/java/cc/abbie/carpetskyadditionsemi/EmiPlugin.java
@@ -4,12 +4,12 @@ import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.recipe.EmiWorldInteractionRecipe;
 import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.potion.PotionUtil;
-import net.minecraft.potion.Potions;
-import net.minecraft.registry.tag.ItemTags;
-import net.minecraft.util.Identifier;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.alchemy.Potions;
 
 public class EmiPlugin implements dev.emi.emi.api.EmiPlugin {
     public static final String MODID = "csaemi";
@@ -18,15 +18,15 @@ public class EmiPlugin implements dev.emi.emi.api.EmiPlugin {
     public void register(EmiRegistry registry) {
         // Thick Potions Convert Stone into Deepslate
         registry.addRecipe(EmiWorldInteractionRecipe.builder()
-                .id(new Identifier(MODID, "thick"))
+                .id(new ResourceLocation(MODID, "thick"))
                 .leftInput(EmiStack.of(Items.STONE))
-                .rightInput(EmiStack.of(PotionUtil.setPotion(new ItemStack(Items.POTION), Potions.THICK)), false)
+                .rightInput(EmiStack.of(PotionUtils.setPotion(new ItemStack(Items.POTION), Potions.THICK)), false)
                 .output(EmiStack.of(Items.DEEPSLATE))
                 .build());
 
         // Saplings Die on Sand
         registry.addRecipe(EmiWorldInteractionRecipe.builder()
-                .id(new Identifier(MODID, "die"))
+                .id(new ResourceLocation(MODID, "die"))
                 .leftInput(EmiIngredient.of(ItemTags.SAPLINGS))
                 .rightInput(EmiIngredient.of(ItemTags.SAND), true)
                 .output(EmiStack.of(Items.DEAD_BUSH))
@@ -34,7 +34,7 @@ public class EmiPlugin implements dev.emi.emi.api.EmiPlugin {
 
         // Anvils Compact Coal into Diamonds
         registry.addRecipe(EmiWorldInteractionRecipe.builder()
-                .id(new Identifier(MODID, "compact"))
+                .id(new ResourceLocation(MODID, "compact"))
                 .leftInput(EmiStack.of(Items.COAL_BLOCK, 64))
                 .rightInput(EmiStack.of(Items.ANVIL), true)
                 .output(EmiStack.of(Items.DIAMOND))
